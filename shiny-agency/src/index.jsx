5e74b6b87372a1aff6a017cda9ebf68b064202ea
Natalia Cherman <natalia.cherman@hainaut-ea.be>
2026-09-24 17:08:39 +0200

    modif freelances et results

--- shiny-agency/src/index.jsx
+++ shiny-agency/src/index.jsx
@@ -4,6 +4,8 @@ import Home from './pages/Home';
 import Survey from './pages/Survey';
 import Header from './components/Header';
 import Error from './components/Error';
+import Results from './pages/Results';
+import Freelances from './pages/Freelances';
 
 const root = createRoot(document.getElementById('root'));
 
@@ -13,6 +15,8 @@ root.render(
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/survey/:questionNumber" element={<Survey />} />
+      <Route path="/results" element={<Results />} />
+      <Route path="/freelances" element={<Freelances />} />
       <Route path="*" element={<Error />} />
     </Routes>
   </Router>,
