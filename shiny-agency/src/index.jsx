0fdbd84c180fb81bf463e37ea95c672ae087613f
Natalia Cherman <natalia.cherman@hainaut-ea.be>
2026-09-24 16:03:00 +0200

    Test outlets

--- shiny-agency/src/index.jsx
+++ shiny-agency/src/index.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Survey from './pages/Survey';
 import Header from './components/Header';
+// On ajoute nos composants
+import ClientForm from './components/ClientForm';
+import FreelanceForm from './components/FreelanceForm';
 
 const root = createRoot(document.getElementById('root'));
 
@@ -11,7 +14,11 @@ root.render(
     <Header />
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/survey" element={<Survey />} />
+      <Route path="/survey" element={<Survey />}>
+        {/* Nous imbriquons nos composants dans survey */}
+        <Route path="client" element={<ClientForm />} />
+        <Route path="freelance" element={<FreelanceForm />} />
+      </Route>
     </Routes>
   </Router>,
 );
