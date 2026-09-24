b8c130967a57dd3e8e6981abee22fbabd789c8ae
Natalia Cherman <natalia.cherman@hainaut-ea.be>
2026-09-24 19:00:44 +0200

    PropTypes et StyledComponent

--- shiny-agency/src/components/Error/index.jsx
+++ shiny-agency/src/components/Error/index.jsx
@@ -1,8 +1,37 @@
+import styled from 'styled-components';
+import colors from '../../utils/style/color';
+import Illustration404 from '../../assets/404.svg';
+
+const ErrorWrapper = styled.div`
+  margin: 30px;
+  display: flex;
+  flex-direction: column;
+  background-color: ${colors.background};
+  align-items: center;
+`;
+
+const ErrorTitle = styled.h1`
+  font-weight: 300;
+`;
+
+const ErrorSubtitle = styled.h2`
+  font-weight: 300;
+  color: ${colors.secondary};
+`;
+
+const Illustration = styled.img`
+  max-width: 800px;
+`;
+
 function Error() {
   return (
-    <div>
-      <h1>Oups, cette page n'existe pas</h1>
-    </div>
+    <ErrorWrapper>
+      <ErrorTitle>Oups...</ErrorTitle>
+      <Illustration src={Illustration404} alt="Page introuvable" />
+      <ErrorSubtitle>
+        Il semblerait que la page que vous cherchez n’existe pas
+      </ErrorSubtitle>
+    </ErrorWrapper>
   );
 }
 
--- shiny-agency/src/index.jsx
+++ shiny-agency/src/index.jsx
@@ -2,15 +2,27 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Survey from './pages/Survey';
-import Header from './components/Header';
-import Error from './components/Error';
 import Results from './pages/Results';
 import Freelances from './pages/Freelances';
+import Header from './components/Header';
+import Error from './components/Error';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        font-family: 'Trebuchet MS', Helvetica, sans-serif;
+    }
+
+    body {
+        margin : 0;
+    }
+`;
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
   <Router>
+    <GlobalStyle />
     <Header />
     <Routes>
       <Route path="/" element={<Home />} />
